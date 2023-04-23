7a27ccba3f34ea88e6efb75208bba7fbe7277b7c
dende-h <miyuki_dende@yahoo.co.jp>
2023-04-24 06:52:11 +0900

    localStorage→localforage

--- src/Atoms/heartNovelArray.tsx
+++ src/Atoms/heartNovelArray.tsx
@@ -1,9 +1,17 @@
+import localforage from "localforage";
 import { atom } from "recoil";
-import { recoilPersist } from "recoil-persist";
+import { recoilPersist } from "../components/customRecoilPersist";
+
+localforage.config({
+	driver: localforage.INDEXEDDB,
+	name: "heartNovelArray",
+	version: 2,
+	storeName: "heartNovelArray"
+});
 
 const { persistAtom } = recoilPersist({
 	key: "recoil-persist",
-	storage: typeof window === "undefined" ? undefined : localStorage
+	storage: typeof window === "undefined" ? undefined : localforage
 });
 
 const defaultValue: string[] = [];
