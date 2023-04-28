8ef88dfabd19cf63c0bfd55f6ce9f98266d2e815
Hiroya Shitogiden <dende.capo@gmail.com>
2023-04-28 15:25:06 +0900

    eslintrc編集

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,21 +3,12 @@ import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import { RecoilRoot } from "recoil";
 import GoogleAnalytics from "../components/GoogleAnalytics";
 import theme from "../theme";
-import { AppProps } from "next/app";
+import { type AppProps } from "next/app";
 import usePageView from "../hooks/usePageView";
 import { useEffect } from "react";
 const SiteKey = process.env.NEXT_PUBLIC_GOOGLE_RECAOTCHA_KEY;
 
 function MyApp({ Component, pageProps }: AppProps) {
-	useEffect(() => {
-		if ("serviceWorker" in navigator) {
-			navigator.serviceWorker.getRegistrations().then((registrations) => {
-				for (const registration of registrations) {
-					registration.unregister();
-				}
-			});
-		}
-	}, []);
 	usePageView();
 	return (
 		<ChakraProvider theme={theme}>
