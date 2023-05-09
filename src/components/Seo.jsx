9a9ada69d443ef32adea7845b70e968a332fad0f
Hiroya Shitogiden <dende.capo@gmail.com>
2023-05-09 10:35:43 +0900

    SEOコンポーネント編集

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -24,6 +24,7 @@ const Seo = ({ pageTitle, pageDescription, pagePath, pageImg, pageImgWidth, page
 			<meta name="viewport" content="width=device-width,initial-scale=1.0" />
 			<meta name="description" content={description} />
 			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:title" content={description} />
 			<meta property="og:url" content={url} />
 			<meta property="og:title" content={title} />
 			<meta property="og:site_name" content={title} />
