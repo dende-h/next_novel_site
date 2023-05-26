6b09ae1ab8d5a1f246885b438d5cd706f86bfbba
Hiroya Shitogiden <dende.capo@gmail.com>
2023-05-26 11:05:16 +0900

    WEBhook実装

---
+++ src/pages/api/revalidate/[id].ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+	revalidated: boolean;
+};
+
+type Msg = {
+	message: string;
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data | Msg>) {
+	console.log("Revalidating notes pages...");
+	const {
+		query: { id }
+	} = req;
+
+	// シークレットキーがないと処理しない
+	if (req.query.secret !== process.env.REVALIDATE_SECRET) {
+		return res.status(401).json({ message: "Your secret is invalid !" });
+	}
+
+	let revalidated = false;
+	try {
+		await res.revalidate(`/novels/${id}`);
+		revalidated = true;
+	} catch (e) {
+		console.log(e);
+	}
+	res.status(200).json({ revalidated });
+}
