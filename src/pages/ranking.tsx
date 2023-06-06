2ee801da0df4877381d5273af2892819453b26ee
Hiroya Shitogiden <dende.capo@gmail.com>
2023-06-06 17:26:56 +0900

    seo追加

--- src/pages/ranking.tsx
+++ src/pages/ranking.tsx
@@ -22,6 +22,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Header from "../components/Header";
 import { Footer } from "../components/Footer";
+import Seo from "../components/Seo";
 
 type RankingData = {
 	pagePath: string;
@@ -96,98 +97,108 @@ export default function RankingPage() {
 	const isSmallScreen = useBreakpointValue({ base: true, md: false });
 
 	return (
-		<Box minH="100vh" display="flex" flexDirection="column">
-			<Header />
+		<>
+			<Seo
+				pageTitle="ランキング"
+				pageDescription="期間別PV数のランキングを閲覧できます"
+				pageImg={null}
+				pagePath="https://next-novel-site.vercel.app/ranking"
+				pageImgHeight="600"
+				pageImgWidth="1200"
+			/>
+			<Box minH="100vh" display="flex" flexDirection="column">
+				<Header />
 
-			<Container flex="1" maxW={"100%"} py={8} px={4}>
-				<Box maxW={"7xl"} mx={"auto"} pt={5} px={{ base: 2, sm: 5, md: 7 }}>
-					<Heading
-						as="h1"
-						mb={4}
-						textAlign={"center"}
-						fontSize={{ base: "2xl", md: "3xl", lg: "4xl" }}
-						color={"gray.700"}
-					>
-						Novel Ranking
-					</Heading>
-					<Box borderRadius="lg" overflow="hidden" boxShadow="lg" bg="gray.100" p={5}>
-						{isSmallScreen ? (
-							<Select defaultValue={"allTime"} mb={4} onChange={(e) => handleTabsChange(e.target.selectedIndex)}>
-								<option value="allTime">All Time</option>
-								<option value="monthly">Last 30 days</option>
-								<option value="weekly">Last 7 days</option>
-								<option value="daily">Last 24 hours</option>
-							</Select>
-						) : (
-							<Tabs variant="enclosed" index={tabIndex} onChange={handleTabsChange}>
-								<TabList borderColor={"gray.500"}>
-									<Tab>All Time</Tab>
-									<Tab>Last 30 days</Tab>
-									<Tab>Last 7 days</Tab>
-									<Tab>Last 24 hours</Tab>
-								</TabList>
-							</Tabs>
-						)}
-						<Box bg="white" borderRadius="md" borderWidth="1px" borderColor="gray.300" overflow="hidden" mt={4}>
-							<Table variant="simple" size="sm">
-								<Thead bg="gray.50">
-									<Tr>
-										<Th textAlign="center" color="gray.500" fontWeight="semibold">
-											Ranking
-										</Th>
-										{!isSmallScreen && (
+				<Container flex="1" maxW={"100%"} py={8} px={4}>
+					<Box maxW={"7xl"} mx={"auto"} pt={5} px={{ base: 2, sm: 5, md: 7 }}>
+						<Heading
+							as="h1"
+							mb={4}
+							textAlign={"center"}
+							fontSize={{ base: "2xl", md: "3xl", lg: "4xl" }}
+							color={"gray.700"}
+						>
+							Novel Ranking
+						</Heading>
+						<Box borderRadius="lg" overflow="hidden" boxShadow="lg" bg="gray.100" p={5}>
+							{isSmallScreen ? (
+								<Select defaultValue={"allTime"} mb={4} onChange={(e) => handleTabsChange(e.target.selectedIndex)}>
+									<option value="allTime">All Time</option>
+									<option value="monthly">Last 30 days</option>
+									<option value="weekly">Last 7 days</option>
+									<option value="daily">Last 24 hours</option>
+								</Select>
+							) : (
+								<Tabs variant="enclosed" index={tabIndex} onChange={handleTabsChange}>
+									<TabList borderColor={"gray.500"}>
+										<Tab>All Time</Tab>
+										<Tab>Last 30 days</Tab>
+										<Tab>Last 7 days</Tab>
+										<Tab>Last 24 hours</Tab>
+									</TabList>
+								</Tabs>
+							)}
+							<Box bg="white" borderRadius="md" borderWidth="1px" borderColor="gray.300" overflow="hidden" mt={4}>
+								<Table variant="simple" size="sm">
+									<Thead bg="gray.50">
+										<Tr>
 											<Th textAlign="center" color="gray.500" fontWeight="semibold">
-												Image
+												Ranking
 											</Th>
-										)}
-										<Th textAlign="center" color="gray.500" fontWeight="semibold">
-											Information
-										</Th>
-										<Th textAlign="center" color="gray.500" fontWeight="semibold">
-											PV Count
-										</Th>
-									</Tr>
-								</Thead>
-								<Tbody>
-									{rankingData.slice(0, 50).map((item, index) => (
-										<Tr key={index}>
-											<Td textAlign="center">{index + 1}</Td>
-											{!isSmallScreen &&
-												(item.imageUrl ? (
-													<Td display="flex" justifyContent="center">
-														<Image alt={"image"} src={item.imageUrl} width={45} height={73} />
-													</Td>
-												) : (
-													<Td textAlign="center">
-														<Text>No Image</Text>
-													</Td>
-												))}
-											<Td>
-												<VStack align="start" spacing={1}>
-													<Link href={`${baseURL}${item.pagePath}`}>
-														<Text fontWeight="bold" color="blue" fontSize={{ base: "11px", md: "12px", lg: "14px" }}>
-															{item.title}
-														</Text>
-													</Link>
-													<Text fontSize="sm" color="gray.500">
-														by {item.userName}
-													</Text>
-												</VStack>
-											</Td>
-											<Td textAlign="center">{item.uniquePageviews}</Td>
+											{!isSmallScreen && (
+												<Th textAlign="center" color="gray.500" fontWeight="semibold">
+													Image
+												</Th>
+											)}
+											<Th textAlign="center" color="gray.500" fontWeight="semibold">
+												Information
+											</Th>
+											<Th textAlign="center" color="gray.500" fontWeight="semibold">
+												PV Count
+											</Th>
 										</Tr>
-									))}
-								</Tbody>
-							</Table>
+									</Thead>
+									<Tbody>
+										{rankingData.slice(0, 50).map((item, index) => (
+											<Tr key={index}>
+												<Td textAlign="center">{index + 1}</Td>
+												{!isSmallScreen &&
+													(item.imageUrl ? (
+														<Td display="flex" justifyContent="center">
+															<Image alt={"image"} src={item.imageUrl} width={45} height={73} />
+														</Td>
+													) : (
+														<Td textAlign="center">
+															<Text>No Image</Text>
+														</Td>
+													))}
+												<Td>
+													<VStack align="start" spacing={1}>
+														<Link href={`${baseURL}${item.pagePath}`}>
+															<Text fontWeight="bold" color="blue" fontSize={{ base: "11px", md: "12px", lg: "14px" }}>
+																{item.title}
+															</Text>
+														</Link>
+														<Text fontSize="sm" color="gray.500">
+															by {item.userName}
+														</Text>
+													</VStack>
+												</Td>
+												<Td textAlign="center">{item.uniquePageviews}</Td>
+											</Tr>
+										))}
+									</Tbody>
+								</Table>
+							</Box>
 						</Box>
 					</Box>
-				</Box>
-			</Container>
+				</Container>
 
-			<Box bg="gray.900" color="white" py={4}>
-				{/* フッター */}
-				<Footer />
+				<Box bg="gray.900" color="white" py={4}>
+					{/* フッター */}
+					<Footer />
+				</Box>
 			</Box>
-		</Box>
+		</>
 	);
 }
