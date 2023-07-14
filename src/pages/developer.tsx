9f589af769e9a5ea762f53c243109a6aaf51d6f7
Hiroya Shitogiden <dende.capo@gmail.com>
2023-07-14 11:47:05 +0900

    更新情報追加

--- src/pages/developer.tsx
+++ src/pages/developer.tsx
@@ -102,6 +102,13 @@ const ShortNovelVillage: React.FC = () => {
 							<ListItem mb={1}>
 								<HStack spacing={0}>
 									<ListIcon as={MdSettings} color="green.500" />
+									<Text fontWeight={"bold"}>23/07/14</Text>
+								</HStack>
+								<Text>一覧から個別頁へ遷移後、ブラウザバック時に元の位置に戻るように修正</Text>
+							</ListItem>
+							<ListItem mb={1}>
+								<HStack spacing={0}>
+									<ListIcon as={MdSettings} color="green.500" />
 									<Text fontWeight={"bold"}>23/07/13</Text>
 								</HStack>
 								<Text>一部のユーザーの小説一覧ページへ遷移出来ない問題の修正、ポリシー修正、開発者紹介追加</Text>
